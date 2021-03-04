2fe6d7dcf4aa8f5d263b965288b463c6a03927d8
Akash J Nair <akashjnair701@gmail.com>
2021-03-04 08:50:05 +0530

    qclass added

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Questions extends StatefulWidget {
   @override
@@ -9,14 +10,37 @@ class _QuestionsState extends State<Questions> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Text('Question'),
-            ),
-            Icon(Icons.priority_high_sharp),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Text(
+                'Question No 7',
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.minimize_rounded,
+                    size: 50,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(child: Text(''))
+            ],
+          ),
         ),
       ),
     );
